c3dbde4678ce03f37fe740b3c88175014cefd453
Pakjetoshka <frostcraftnumber1@gmail.com>
2024-11-15 23:58:08 +0200

    15.11.24 - hw7 - task3 plsssss

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,14 @@
 const textInput = document.querySelector("#name-input");
 const spanOutput = document.querySelector("#name-output");
 
-textInput.addEventListener("input", (event) => {
-    spanOutput.textContent = event.currentTarget.value.trim();
-});
 textInput.addEventListener("blur", (event) => {
     textInput.value = event.currentTarget.value.trim();
 });
-
 textInput.addEventListener("input", setOutput);
-function setOutput() {
-    if (spanOutput.textContent === "")
+
+function setOutput(event) {
+    spanOutput.textContent = event.currentTarget.value.trim();
+    if (textInput.value.trim() === "")
     {
         spanOutput.textContent = "Anonymous";
     }
